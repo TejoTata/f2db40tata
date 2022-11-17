6212858ab46568e724130e01eeebb011d45dbb47
Tejo <111945716+TejoTata@users.noreply.github.com>
2022-11-16 19:09:02 -0600

    added styles

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -79,7 +79,7 @@ exports.vehicle_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"vehicle_type":"goat", "cost":12, "size":"large"}
+    // {"vehicle_type":"goat", "cost":12, "price":"large"}
     document.vehicle_name = req.body.vehicle_name;
     document.vehicle_type = req.body.vehicle_type;
     document.vehicle_price = req.body.vehicle_price;
@@ -93,3 +93,98 @@ exports.vehicle_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }
+// for a specific vehicle.
+exports.vehicle_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await vehicle.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+   // Handle vehicle update form on PUT.
+exports.vehicle_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await vehicle.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.vehicle_name)
+ toUpdate.vehicle_name = req.body.vehicle_name;
+ if(req.body.vehicle_type) toUpdate.vehicle_type = req.body.vehicle_type;
+ if(req.body.vehicle_price) toUpdate.vehicle_price = req.body.vehicle_price;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
+// Handle vehicle delete on DELETE.
+exports.vehicle_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await vehicle.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+// Handle a show one view with id specified by query
+exports.vehicle_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await vehicle.findById( req.query.id)
+    res.render('vehicledetail',
+    { title: 'vehicle Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for creating a vehicle.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vehicle_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('vehiclecreate', { title: 'vehicle Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for updating a vehicle.
+// query provides the id
+exports.vehicle_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await vehicle.findById(req.query.id)
+    res.render('vehicleupdate', { title: 'vehicle Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.vehicle_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await vehicle.findById(req.query.id)
+    res.render('vehicledelete', { title: 'vehicle Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -11,3 +11,13 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 module.exports = router;
+// GET request for one vehicle.
+router.get('/vehicle/:id', vehicle_controlers.vehicle_detail);
+/* GET detail vehicle page */
+router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
+/* GET create vehicle page */
+router.get('/create', vehicle_controlers.vehicle_create_Page);
+/* GET create update page */
+router.get('/update', vehicle_controlers.vehicle_update_Page);
+/* GET delete vehicle page */
+router.get('/delete', vehicle_controlers.vehicle_delete_Page);
